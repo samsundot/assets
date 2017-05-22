4a93870951b6b13d16b4a4121edfdfe64a6d8e47
hflyf123 <hflyf123@qq.com>
2017-05-22 17:21:53 +0800

    123

--- src/test/java/com/assets/AssetsApplicationTests.java
+++ src/test/java/com/assets/AssetsApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AssetsApplicationTests {
-
+//123
 	@Test
 	public void contextLoads() {
 	}
